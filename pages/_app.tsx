22b373a8af97bd8c8d2c90bda3986b11ea779d01
Jakub Kuś <jakub@jkus.dev>
2024-01-30 22:03:15 +0100

    change theme to darcula

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
-import "prism-themes/themes/prism-dracula.css";
+import "prism-themes/themes/prism-darcula.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

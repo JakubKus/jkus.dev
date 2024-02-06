25f8f06600b589a902d5f302ff5c547b56901ae8
Jakub Kuś <jakub@jkus.dev>
2024-02-06 21:43:40 +0100

    change img to Image component, to optimize avatar image

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 const author = {
   picture: '/assets/blog/author/jakub.jpg',
   bioLines: [
@@ -9,7 +11,13 @@ const author = {
 const Avatar = () => {
   return (
     <div className="flex items-center">
-      <img src={author.picture} className="w-12 h-12 rounded-full mr-4" alt="author image" />
+      <Image
+        src={author.picture}
+        className="w-12 h-12 rounded-full mr-4"
+        alt="author (Jakub Kuś) profile picture"
+        height={48}
+        width={48}
+      />
       <div>
         {author.bioLines.map((line) => (
           <p key={line} className="text-base leading-7 tracking-wide">{line}</p>

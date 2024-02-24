987cde7bc5ff866e2df9db0d016e4277912ae433
Jakub Kuś <jakub@jkus.dev>
2024-02-24 18:40:35 +0100

    add reading progress

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,17 +2,21 @@ import Meta from './meta';
 import React, { PropsWithChildren } from 'react';
 import Footer from './footer';
 import Navbar from './navigation/navbar';
+import ReadingProgress from './reading-progress';
 
 const Layout = ({ children }: PropsWithChildren) => {
+  const contentRef = React.createRef<HTMLDivElement>();
+
   return (
-    <>
+    <div ref={contentRef}>
       <Meta />
       <Navbar />
-      <div className="mt-navbar wideDesktop:mt-desktopNavbar py-8 min-h-content wideDesktop:min-h-desktopContent">
-        <main>{children}</main>
-      </div>
+      <ReadingProgress contentRef={contentRef} />
+      <main className="mt-navbar wideDesktop:mt-desktopNavbar py-8 min-h-content wideDesktop:min-h-desktopContent">
+        {children}
+      </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
---
+++ components/reading-progress.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type Props = {
+  contentRef: React.RefObject<HTMLDivElement>;
+};
+
+const ReadingProgress = ({ contentRef }: Props) => {
+  const [readingProgress, setReadingProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const scrollListener = () => {
+      if (!contentRef.current) return;
+
+      const element = contentRef.current;
+      const contentHeight = element.clientHeight + element.offsetTop - window.innerHeight;
+      const windowScrollPosition = window.scrollY;
+
+      if (windowScrollPosition === 0) return setReadingProgress(0);
+      if (windowScrollPosition > contentHeight) return setReadingProgress(100);
+
+      setReadingProgress(windowScrollPosition / contentHeight);
+    };
+
+    document.addEventListener('scroll', scrollListener);
+
+    return () => {
+      document.removeEventListener('scroll', scrollListener);
+    }
+  }, [contentRef]);
+
+  return (
+    <span
+      className={`w-full h-[1px] origin-left bg-accent-2 fixed top-navbar desktop:top-desktopNavbar`}
+      style={{ transform: `scaleX(${readingProgress})` }}
+    />
+  );
+}
+
+export default ReadingProgress;

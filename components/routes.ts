07a835fd794e9921ae31bb63873225dbfad7f104
Jakub Kuś <jakub@jkus.dev>
2025-02-21 12:52:05 +0100

    remove projects page

--- components/routes.ts
+++ components/routes.ts
@@ -1,7 +1,6 @@
 export const routes = [
   { link: '/', label: 'ABOUT ME' },
   { link: '/blog', label: 'BLOG' },
-  { link: '/projects', label: 'PROJECTS' },
   { link: '/books', label: 'BOOKS' },
   { link: '/tools', label: 'TOOLS' },
 ];

ec869d08cdc671aef1474b4081c0f6cb86fea995
Jakub Kuś <jakub@jkus.dev>
2026-03-30 23:12:04 +0200

    replace old about me page with new one

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  async rewrites() {
+    return [{ source: '/', destination: '/index.html' }];
+  },
 };
 
 module.exports = nextConfig;

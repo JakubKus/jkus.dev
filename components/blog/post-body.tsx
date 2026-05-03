b2d944038f8526eb83622b33f048a54499076909
Jakub Kuś <jakub@jkus.dev>
2026-05-03 22:00:55 +0200

    remove avatar with bio from post body

--- components/blog/post-body.tsx
+++ components/blog/post-body.tsx
@@ -1,5 +1,4 @@
 import markdownStyles from './markdown-styles.module.css';
-import AvatarWithBio from '../avatar-with-bio';
 import type { PostLinkType } from '../../interfaces/post-link';
 import PostLink from './post-link';
 
@@ -14,7 +13,6 @@ const PostBody = ({ content, prevPost, nextPost }: Props) => {
     <div className="max-w-content mx-auto">
       <div className={markdownStyles['markdown']} dangerouslySetInnerHTML={{ __html: content }} />
       <hr className="border-accent-1-light mb-6" />
-      <AvatarWithBio />
       <ul className="flex justify-between gap-x-3 mt-12">
         <PostLink post={prevPost} prefix="←" />
         <PostLink post={nextPost} suffix="→" alignRight />

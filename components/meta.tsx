ffcebb5eb687da154813f1eb4717c84034b19858
Jakub Kuś <jakub@jkus.dev>
2026-05-03 22:53:39 +0200

    use new favicon in all pages

--- components/meta.tsx
+++ components/meta.tsx
@@ -4,10 +4,10 @@ import { HEADLINE } from '../constants';
 const Meta = () => {
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="400x400" href="/favicon/jakub.jpg" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon.png" />
+      <link rel="apple-touch-icon" sizes="400x400" href="/favicon.svg" />
+      <link rel="icon" type="image/svg+xml" sizes="32x32" href="/favicon.svg" />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="shortcut icon" href="/favicon/favicon.png" />
+      <link rel="shortcut icon" href="/favicon.svg" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
